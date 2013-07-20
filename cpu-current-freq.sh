1ca3757fc1cc967f58352c8f29777963466ec3cf
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-07-20 19:03:08 -0400

    Fixed cpu

--- cpu-current-freq.sh
+++ cpu-current-freq.sh
@@ -2,17 +2,5 @@
 # Simple script to print out the max MHz
 # source http://lwn.net/Articles/379764/
 
-MAX_MHZ=0
-SYSFS_SCALING=/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies
-
-# Use sysfs if available
-if [ -e $SYSFS_SCALING ]; then
-    for CURR_MHZ in `cat $SYSFS_SCALING`; do
-        CURR_MHZ=$(($CURR_MHZ/1000))
-        if [ $CURR_MHZ -gt $MAX_MHZ ]; then
-            MAX_MHZ=$CURR_MHZ
-        fi
-    done
-    echo $MAX_MHZ
-    exit 0
-fi
+grep "MHz" /proc/cpuinfo  | grep -m1 -oP "\d+\.\d+"
+exit 0
--- install.sh
+++ install.sh
@@ -10,6 +10,12 @@ if [ -f ../.git-prompt.sh ]
 then
     mv ../.git-prompt.sh ../.git-prompt.sh.bak
 fi
+if [ -f ../.xinitrc ]
+then
+    mv ../.xinitrc ../.xinitrc.bak
+fi
 
 ln _bashrc ../.bashrc
 ln _git-prompt.sh ../.git-prompt.sh
+ln _xinitrc ../.xinitrc
+ln _xinitrc ../.Xsession
