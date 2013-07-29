b6c9e86f89e0e4b3736298de80c5d728787b1f92
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-07-29 15:33:54 -0400

    Added tmux config

--- install.sh
+++ install.sh
@@ -14,8 +14,13 @@ if [ -f ../.xinitrc ]
 then
     mv ../.xinitrc ../.xinitrc.bak
 fi
+if [ -f ../.tmux.conf ]
+then
+    mv ../.tmux.conf ../.tmux.conf.bak
+fi
 
 ln _bashrc ../.bashrc
 ln _git-prompt.sh ../.git-prompt.sh
 ln _xinitrc ../.xinitrc
 ln _xinitrc ../.Xsession
+ln _tmux.conf ../.tmux.conf
